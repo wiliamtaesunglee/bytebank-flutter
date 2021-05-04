1fb3a5ef23185541e0ab2b29691e5054c4945067
guts <wiliam.tae@gmail.com>
2021-05-04 10:36:51 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(
+  MaterialApp(
+    home: Scaffold(
+      body: Column(children: [
+        Card(
+          child: ListTile(
+            leading: Icon(Icons.monetization_on),
+            title: Text('100,00'),
+            subtitle: Text('1000'),
+          )
+        ),
+        Card(
+          child: ListTile(
+            leading: Icon(Icons.monetization_on),
+            title: Text('100,00'),
+            subtitle: Text('1000'),
+          )
+        ),
+      ],),
+      appBar: AppBar(
+        title: Text('Transferências'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => {},
+        child: Icon(Icons.add),
+      ),
+    ),
+  ),
+);
